f75610b486f246d0c13df12a2043f30a16a1b1a1
Rick Chang <rick800721@gmail.com>
2024-10-02 20:58:10 +0100

    Switch db from Sqlite to Postgresql

--- WellenceChallenge/WellenceChallenge/settings.py
+++ WellenceChallenge/WellenceChallenge/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "WellenceChallenge.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "WellenceChallenge",
+        "USER": "postgres",
+        "PASSWORD": "test",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
